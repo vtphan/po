6cda6b8d565c7e8b1ac6346994d0bcaca1de07dd
vtphan <vphan@memphis.edu>
2015-09-13 10:20:24 -0500

    .

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ setup(
    name='po',
    version='0.1',
    py_modules=['po'],
-   install_requires=['pandas','seaborn','scikit-learn','matplotlib']
+   install_requires=['pandas','seaborn','scikit-learn','matplotlib'],
+   description='Po - a convenient package for analyzing tabular data.',
+   author='Vinhthuy Phan'
 )
